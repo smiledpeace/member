644b14873146cadcb2cdf05a1f7ef31aa8c5b55c
smiledpeace <863899253@qq.com>
2019-03-20 10:38:47 +0800

    all

---
+++ public/mobile/nativeApps/test.vue
@@ -0,0 +1,23 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "test",
+        created() {
+        },
+        mounted() {
+        },
+        data() {
+            return {}
+        },
+        methods: {},
+        props: {},
+        components: {},
+    }
+</script>
+
+<style lang="less" rel="stylesheet/less">
+
+</style>
