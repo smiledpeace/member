7884c2c147292dcea1fb7f36b153fd5e6c24e2d7
smiledpeace <863899253@qq.com>
2019-03-21 17:30:54 +0800

    member

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('web/common/index', { title: 'Express' });
+  res.render('web/common/index', { title: 'Hello World' });
 });
 
 module.exports = router;
